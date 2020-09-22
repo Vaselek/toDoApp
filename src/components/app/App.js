6ec8a853114e7111ef559b1ead95ff4bbc460d2b
Vaselek <burzhuas@gmail.com>
2020-09-22 13:47:57 +0600

    Add bootstrap

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button>hi</Button>
       The app
     </div>
   );
